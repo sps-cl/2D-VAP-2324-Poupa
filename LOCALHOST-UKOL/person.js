8a9e8b94ae95756fe2c0c22a27e630fe4732a33a
FrantisekPoupa <frantisek.poupa@gmail.com>
2024-03-26 15:26:04 +0100

    Oprava úkolu

---
+++ LOCALHOST-UKOL/person.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(firstName, lastName, id) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.id = id;
+    }
+  }
+  
+  module.exports = Person;
