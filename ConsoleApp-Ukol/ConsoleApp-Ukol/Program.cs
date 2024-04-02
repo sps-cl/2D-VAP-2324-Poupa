68649b6d9f41b69f47424e15c03784f53cb2f544
FrantisekPoupa <frantisek.poupa@gmail.com>
2024-04-02 16:32:16 +0200

    Nahrání úkolu C#

---
+++ ConsoleApp-Ukol/ConsoleApp-Ukol/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Ukol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cisla;
+
+            while (true)
+            {
+                Console.WriteLine("Zadej sekvenci čísel oddělených čárkou:");
+                string vstup = Console.ReadLine();
+
+                try
+                {
+                    cisla = vstup.Split(',').Select(int.Parse).ToArray();
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Chybný formát vstupu. Zadej čísla znovu.");
+                }
+            }
+
+            int min = cisla.Min();
+            int max = cisla.Max();
+            int nejdelsiVzestupnyInterval = vzestupnyinterval(cisla);
+
+            Console.WriteLine($"Nejmenší číslo je {min}.");
+            Console.WriteLine($"Největší číslo je {max}.");
+            Console.WriteLine($"Délka nejdelšího vzestupného intervalu je {nejdelsiVzestupnyInterval}.");
+        }
+
+        static int vzestupnyinterval(int[] cisla)
+        {
+            int maxdelka = 0;
+            int momentalnidelka = 1;
+
+            for (int i = 1; i < cisla.Length; i++)
+            {
+                if (cisla[i] > cisla[i - 1])
+                {
+                    momentalnidelka++;
+                }
+                else
+                {
+                    maxdelka = Math.Max(maxdelka, momentalnidelka);
+                    momentalnidelka = 1;
+                }
+            }
+
+            maxdelka = Math.Max(maxdelka, momentalnidelka);
+
+            return maxdelka;
+        }
+    }
+}
