98129b0f5c9b2fb19d0f7fac4aae3012ec23ae4e
FrantisekPoupa <frantisek.poupa@gmail.com>
2024-04-23 16:34:20 +0200

    3. konzolová aplikace

---
+++ CS_UKOL3/CS_UKOL3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ConsoleApp
+{
+    private List<string> words = new List<string>();
+    private int currentIndex = -1;
+
+    void AddWord(string word)
+    {
+        words.Add(word);
+        currentIndex = words.Count - 1;
+        Console.WriteLine(word);
+    }
+
+    void GoBack()
+    {
+        if (currentIndex > 0)
+        {
+            currentIndex--;
+        }
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    void GoForward()
+    {
+        if (currentIndex < words.Count - 1)
+        {
+            currentIndex++;
+        }
+        Console.WriteLine(words[currentIndex]);
+    }
+
+    void Run()
+    {
+        while (true)
+        {
+            Console.Write("Vstup: ");
+            string command = Console.ReadLine();
+
+            if (command.StartsWith("Pridat:"))
+            {
+                Console.Write("Vystup: ");
+                string word = command.Substring(7);
+                AddWord(word);
+            }
+            else if (command == "Zpet")
+            {
+                Console.Write("Vystup: ");
+                GoBack();
+            }
+            else if (command == "Vpred")
+            {
+                Console.Write("Vystup: ");
+                GoForward();
+            }
+            else
+            {
+                Console.WriteLine("Neznámý příkaz.");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        ConsoleApp app = new ConsoleApp();
+        app.Run();
+    }
+}
